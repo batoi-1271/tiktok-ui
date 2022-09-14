ab10ea2ced4d6c29b8f4798c66fbf58857c46c42
Ba Toi Nguyen <batoi1271.doe@gmail.com>
2022-09-14 22:26:51 +0700

    CSS/SASS & prettier

---
+++ config-overrides.js
@@ -0,0 +1,4 @@
+module.exports = function override(config, env) {
+    // do stuff with the config...
+    return config;
+}
--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
+import GlobalStyles from './components/GlobalStyles';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <GlobalStyles>
+      <App />
+    </GlobalStyles>
+  </React.StrictMode>,
 );
-
-
